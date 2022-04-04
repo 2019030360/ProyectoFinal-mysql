573ff752885731a6aec27c63881dc5c02cd47278
2019030360 <2019030360@uposin.edu.mx>
2022-04-04 10:20:55 -0600

    Se agrega el proyecto

--- source/router/index.js
+++ source/router/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dbContactos = require('../models/contactos.js');
 const router = express.Router();
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 router.get('/', (req, res) => {
     res.send("Iniciamos Servidor");
